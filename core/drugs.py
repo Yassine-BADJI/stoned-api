a11741a0ddd63c9f6f1a0dbdfd176f20797b7cdc
Kems <yassine@badji.fr>
2020-05-10 18:58:28 +0200

    Refacto drug path

--- core/drugs.py
+++ core/drugs.py
@@ -1,10 +1,35 @@
-from model import Drugs
+from werkzeug.exceptions import BadRequest
+
+from model import Drugs, Types
+
+
+def check_if_exist(data):
+    if not data:
+        raise BadRequest('No drug or type found!')
 
 
 def get_all_drugs():
     return Drugs.query.all()
 
 
+def get_all_drugs_by(search, value):
+    kwargs = {search: value}
+    return Drugs.query.filter_by(**kwargs).all()
+
+
 def get_drug_by(search, value):
     kwargs = {search: value}
-    return Drugs.query.filter_by(**kwargs).first()
+    drug = Drugs.query.filter_by(**kwargs).first()
+    check_if_exist(drug)
+    return drug
+
+
+def get_all_type():
+    return Types.query.all()
+
+
+def get_type_by(search, value):
+    kwargs = {search: value}
+    type = Types.query.filter_by(**kwargs).first()
+    check_if_exist(type)
+    return type
